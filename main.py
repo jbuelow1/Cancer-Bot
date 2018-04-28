110ee5084ddf5d6eb5ad1261b3e463e15a44b3c4
jbuelow1 <jdbuelow@gmail.com>
2018-04-28 16:57:53 -0500

    Not in working state

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from io import BytesIO
+from PIL import Image
+
+bot = commands.Bot(command_prefix='ifr.')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.event
+async def on_message(message):
+    print('[DEBUG] New Message.')
+    if message.author == bot.user:
+        print('[DEBUG] Message is by me, exiting...')
+        return
+    if message.attachments != []:
+        print('[DEBUG] Message has attachments.')
+        print(Image)
+        return
+    else:
+        print('[DEBUG] Message does not have attachments, exiting...')
+        return
+
+
+bot.run('NDM5ODUxNDU0MjAzNjkxMDE5.DcZQxA.bCCmSP2Q6WkJWFAfFZoGFdXyQ7g')
