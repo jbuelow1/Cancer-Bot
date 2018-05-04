15b7fe17c922661122951950669ffb06c061cfb6
jbuelow1 <jdbuelow@gmail.com>
2018-05-04 11:29:13 -0500

    removed tmp line from debugging last issue

--- main.py
+++ main.py
@@ -96,7 +96,6 @@ def ifukkie_check(suspect):
     res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
     threshold = 0.9
     loc = np.where( res >= threshold)
-    print(str(loc))
     if str(loc) != "(array([], dtype=int64), array([], dtype=int64))":
         return True
     else:
