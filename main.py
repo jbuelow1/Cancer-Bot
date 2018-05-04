787402e53a31d769e3689368ac9114d2fa84572f
jbuelow1 <jdbuelow@gmail.com>
2018-05-04 09:50:25 -0500

    prevented "goodie" from triggering "die"

--- main.py
+++ main.py
@@ -18,7 +18,63 @@ headers={
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 }
 
-silent = False
+pingemojis = [
+'<:squidwardping:441962933208219658>',
+'<:pingwhat:441962933178728448>',
+'<:pingturtle:441962933250031662>',
+'<:pingthink:441962932784332811>',
+'<:PingReee1:441962933099167745>',
+'<:ping:441804270698758155>',
+'<:mokou_ping:441962933623324690>',
+'<:hyperpinged:441962933187248148>',
+'<:GWnoneAngryPing:441962933199831040>',
+'<:FeelsTooPingedMan:441962933199568906>',
+'<:FeelsPingedMan:441962932700446721>',
+'<:drakeping:441962932247724033>',
+'<a:PandaPingRee:441962933115682818>',
+'<a:AniPing:441962933413740545>',
+'<a:angeryping:441962932662829096>'
+]
+
+dancefont = {
+'a': '<a:danceA:440893333150105601>',
+'b': '<a:danceB:440893333443706890>',
+'c': '<a:danceC:440893333301100564>',
+'d': '<a:danceD:440893333460746240>',
+'e': '<a:danceE:440893333204762625>',
+'f': '<a:danceF:440893333502427136>',
+'g': '<a:danceG:440893333439643679>',
+'h': '<a:danceH:440893333355888668>',
+'i': '<a:danceI:440893333586575380>',
+'j': '<a:danceJ:440893333351694346>',
+'k': '<a:danceK:440893333578186752>',
+'l': '<a:danceL:440893333544501258>',
+'m': '<a:danceM:440893333317877760>',
+'n': '<a:danceN:440893333276065813>',
+'o': '<a:danceO:440893333435318272>',
+'p': '<a:danceP:440893333624324113>',
+'q': '<a:danceQ:440893333288517652>',
+'r': '<a:danceR:440893333594832896>',
+'s': '<a:danceS:440893333636775946>',
+'t': '<a:danceT:440893333523398667>',
+'u': '<a:danceU:440893333569667073>',
+'v': '<a:danceV:440893333422997506>',
+'w': '<a:danceW:440893333473198082>',
+'x': '<a:danceX:440893333737439232>',
+'y': '<a:danceY:440893333737308180>',
+'z': '<a:danceZ:440893333917925386>',
+'0': '0',
+'1': '1',
+'2': '2',
+'3': '3',
+'4': '4',
+'5': '5',
+'6': '6',
+'7': '7',
+'8': '8',
+'9': '9'
+}
+
 debug = True
 
 def loglog(message):
@@ -57,21 +113,27 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     debuglog(blankvar.join(('New Message by ', str(message.author), ' with ID ', str(message.id), ' in channel ', str(message.channel.id), '.')))
-    #if message.author == bot.user:
-    #    debuglog('Message is by me, exiting...')
-    #    return
+    if message.author == bot.user:
+        debuglog('Message is by me, exiting...')
+        return
 
-    if (('heck' in message.content) or ('Heck' in message.content) or ('HECK' in message.content) or ('hek' in message.content) or ('Hek' in message.content) or ('HEK' in message.content)):
+    if (('heck' in message.content.lower()) or ('hek' in message.content.lower())):
         debuglog(blankvar.join((str(message.author), ' just said h*ck!')))
-        await bot.send_file(message.channel, blankvar.join(('heck',str(random.randint(1,6)),'.jpg')), content="heck")
+        await bot.send_file(message.channel, blankvar.join(('heck',str(random.randint(1,6)),'.jpg')))
 
     if '@everyone' in message.content:
         debuglog(blankvar.join((str(message.author), ' pinged! REEEE!!!')))
-        await bot.send_file(message.channel, 'madping.png', content="@everyone")
+        await bot.send_message(message.channel, random.choice(pingemojis))
 
-    if (('die' in message.content) or ('Die' in message.content) or ('DIE' in message.content) or ('kys' in message.content) or ('Kys' in message.content) or ('KYS' in message.content) or ('kms' in message.content) or ('Kms' in message.content) or ('KMS' in message.content)):
+    try:
+        if (str1.index('die') == 0 or str1.index(' die ')) or (str1.index('kys') == 0 or str1.index(' kys ')) or (str1.index('kms') == 0 or str1.index(' kms ')):
+            bleach = True
+    except:
+        bleach = False
+
+    if bleach:
         debuglog(blankvar.join((str(message.author), ' wants to die. helping...')))
-        await bot.send_file(message.channel, 'bleach.png', content="kys")
+        await bot.send_file(message.channel, 'bleach.png', content=''.join((dancefont['k'],dancefont['y'],dancefont['s'])))
 
     if message.attachments != []:
         debuglog('Message has attachments. Scanning for cancer...')
@@ -95,7 +157,7 @@ async def on_message(message):
         cancerous = ifukkie
         if cancerous:
             await bot.delete_message(message)
-            await bot.send_message(message.channel, '<:nope:432913100144902146> <a:danceD:440893333460746240><a:danceE:440893333204762625><a:danceL:440893333544501258><a:danceE:440893333204762625><a:danceT:440893333523398667> <a:danceD:440893333460746240><a:danceI:440893333586575380><a:danceS:440893333636775946> <:nope:432913100144902146>')
+            await bot.send_message(message.channel, '<:shooter:441972276901052416> '.join((dancefont['d'],dancefont['e'],dancefont['l'],dancefont['e'],dancefont['t'],' ',dancefont['d'],dancefont['i'],dancefont['s'],' <:shooter:441972276901052416>')))
             loglog('Message was cancerous and was deleted.')
         else:
             debuglog('False Alarm. Message was fine.')
@@ -105,7 +167,7 @@ async def on_message(message):
         debuglog('Message does not have attachments.')
         if 'ifunny.co/' in message.content:
             await bot.delete_message(message)
-            await bot.send_message(message.channel, '<:nope:432913100144902146> <a:danceD:440893333460746240><a:danceE:440893333204762625><a:danceL:440893333544501258><a:danceE:440893333204762625><a:danceT:440893333523398667> <a:danceD:440893333460746240><a:danceI:440893333586575380><a:danceS:440893333636775946> <:nope:432913100144902146>')
+            await bot.send_message(message.channel, '<:shooter:441972276901052416> '.join((dancefont['d'],dancefont['e'],dancefont['l'],dancefont['e'],dancefont['t'],' ',dancefont['d'],dancefont['i'],dancefont['s'],' <:shooter:441972276901052416>')))
             loglog('Message was cancerous and was deleted.')
         return
 
