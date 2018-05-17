95a0cf8658a87e4ec58e4ec0890867ea33082072
jbuelow1 <jdbuelow@gmail.com>
2018-05-17 12:10:53 -0500

    *internet retardation intesnifies*

--- main.py
+++ main.py
@@ -82,24 +82,6 @@ debug = True
 bot = discord.Client()
 
 
-
-
-class ThreadWithReturnValue(Thread):
-    def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs={}, Verbose=None):
-        Thread.__init__(self, group, target, name, args, kwargs, Verbose)
-        self._return = None
-    def run(self):
-        if self._Thread__target is not None:
-            self._return = self._Thread__target(*self._Thread__args,
-                                                **self._Thread__kwargs)
-    def join(self):
-        Thread.join(self)
-        return self._return
-
-
-
-
 def loglog(message):
     print(blankvar.join(('[LOG] [ ] [ ] ', message)))
 
