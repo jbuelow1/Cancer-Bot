8e8ad55acc008261e3e8d5462f507cfc55786ce1
jbuelow1 <jdbuelow@gmail.com>
2018-06-01 12:12:34 -0500

    test

--- main.py
+++ main.py
@@ -440,9 +440,9 @@ async def on_message(message):
 async def on_message_delete(message):
     if message.author == bot.user:
         debuglog('Someone deleted my post! REEEEEEEE Spamming...')
-        await bot.send_message(message.channel, message.content, embed=message.embeds)
-        await bot.send_message(message.channel, message.content, embed=message.embeds)
-        await bot.send_message(message.channel, message.content, embed=message.embeds)
+        await bot.send_message(message.channel, message.content, embed=message.embeds[1])
+        await bot.send_message(message.channel, message.content, embed=message.embeds[1])
+        await bot.send_message(message.channel, message.content, embed=message.embeds[0])
 
 loglog('Attempting to login to Discord...')
 
