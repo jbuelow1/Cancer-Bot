3cd95fa40c0970f3f445f611a2ccf283af96ceb3
jbuelow1 <jdbuelow@gmail.com>
2018-06-05 13:10:49 -0500

    try to delete (for DM control)

--- main.py
+++ main.py
@@ -313,24 +313,28 @@ async def on_message(message):
 
         if message.content.lower().startswith('?/;delete'):
             if str(message.author.id) == '273940917596061698':
-                await bot.delete_message(message)
+                try:
+                    await bot.delete_message(message)
                 target = await bot.get_message(message.channel, message.content.split(' ')[1])
                 await bot.delete_message(target)
 
         if message.content.lower().startswith('?/;adelete'):
             if str(message.author.id) == '273940917596061698':
-                await bot.delete_message(message)
+                try:
+                    await bot.delete_message(message)
                 target = await bot.get_message(message.content.split(' ')[1], message.content.split(' ')[2])
                 await bot.delete_message(target)
 
         if message.content.lower().startswith('?/;say'):
             if str(message.author.id) == '273940917596061698':
-                await bot.delete_message(message)
+                try:
+                    await bot.delete_message(message)
                 await bot.send_message(message.channel, message.content.split(' ', 1)[1])
 
         if message.content.lower().startswith('?/;asay'):
             if str(message.author.id) == '273940917596061698':
-                await bot.delete_message(message)
+                try:
+                    await bot.delete_message(message)
                 await bot.send_message(message.content.split(' ', 2)[1], message.content.split(' ', 2)[2])
 
         #tChIfunny.join()
