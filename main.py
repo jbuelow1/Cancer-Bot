2eb1000e1fd4b2a4f6d26180d6d0830493e98701
Yamcha <jdbuelow@gmail.com>
2018-06-08 14:35:15 -0500

    ?/ping fixes

--- main.py
+++ main.py
@@ -256,12 +256,12 @@ async def on_message(message):
             t1 = time.perf_counter()
             await bot.send_typing(message.channel)
             t2 = time.perf_counter()
-            typingPing = str(round((t2-t1)*1000, 1)) + ' ms'
+            typingPing = str(math.floor((t2-t1)*1000)) + ' ms'
             dnsPing = str(math.floor(get_ping_time('8.8.8.8'))) + ' ms'
             googlePing = str(math.floor(get_ping_time('google.com'))) + ' ms'
             lanPing = str(math.floor(get_ping_time('192.168.1.1'))) + ' ms'
             emPing = discord.Embed(title=':ping_pong: Pong! :ping_pong:')
-            emPing.add_field(name='Typing ping', value=typingPing, inline=True)
+            emPing.add_field(name='Typing ping', value=typingPing, inline=False)
             emPing.add_field(name='DNS ping', value=dnsPing, inline=True)
             emPing.add_field(name='Google ping', value=googlePing, inline=True)
             emPing.add_field(name='LAN ping', value=lanPing, inline=True)
