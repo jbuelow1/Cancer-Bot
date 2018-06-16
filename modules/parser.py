abff61b59097462ddeded6f3eb0ce9f4ccd87f7c
jbuelow1 <jdbuelow@gmail.com>
2018-06-16 18:39:21 -0500

    why arent they awaited all of a sudden?

--- modules/parser.py
+++ modules/parser.py
@@ -12,52 +12,52 @@ class Mparser:
             return
 
         if (findWholeWord('heck')(message.content.lower()) or findWholeWord('hek')(message.content.lower()) or findWholeWord('hecking')(message.content.lower()) or findWholeWord('heckin')(message.content.lower())):
-            await self.bot.send_typing(message.channel)
+            self.bot.send_typing(message.channel)
             self.bot.striggers += 1
             self.bot.utriggers += 1
             emHeck.set_image(url=random.choice(hecks))
-            await self.bot.send_message(message.channel, embed=emHeck)
+            self.bot.send_message(message.channel, embed=emHeck)
 
         if message.mention_everyone:
-            await self.bot.send_typing(message.channel)
+            self.bot.send_typing(message.channel)
             self.bot.striggers += 1
             self.bot.utriggers += 1
-            await self.bot.send_message(message.channel, random.choice(pingemojis))
+            self.bot.send_message(message.channel, random.choice(pingemojis))
 
         if (findWholeWord('die')(message.content.lower()) or findWholeWord('kys')(message.content.lower()) or findWholeWord('kms')(message.content.lower())): #CBP
-            await self.bot.send_typing(message.channel)
+            self.bot.send_typing(message.channel)
             self.bot.striggers += 1
             self.bot.utriggers += 1
             if self.bot.user.mentioned_in(message):
-                await self.bot.send_message(message.channel, 'no u')
-                await self.bot.send_message(message.channel, 'Ladies and gentlmen, I appear to have won this argument. You can stop fighting like little cucklets now.') #CBP
+                self.bot.send_message(message.channel, 'no u')
+                self.bot.send_message(message.channel, 'Ladies and gentlmen, I appear to have won this argument. You can stop fighting like little cucklets now.') #CBP
             else:
-                await self.bot.send_message(message.channel, embed=emBleach)
+                self.bot.send_message(message.channel, embed=emBleach)
 
         elif self.bot.user.mentioned_in(message):
-            await self.bot.send_typing(message.channel)
+            self.bot.send_typing(message.channel)
             self.bot.striggers += 1
             self.bot.utriggers += 1
             if (findWholeWord('die')(message.content.lower()) or findWholeWord('kys')(message.content.lower()) or findWholeWord('kms')(message.content.lower())):
-                await self.bot.send_message(message.channel, 'no u')
-                await self.bot.send_message(message.channel, 'Ladies and gentlmen, I appear to have won this argument. You can stop fighting like little cucklets now.') #CBP
+                self.bot.send_message(message.channel, 'no u')
+                self.bot.send_message(message.channel, 'Ladies and gentlmen, I appear to have won this argument. You can stop fighting like little cucklets now.') #CBP
             else:
-                await self.bot.send_message(message.channel, 'H- Hewwo?!')
+                self.bot.send_message(message.channel, 'H- Hewwo?!')
 
         if findWholeWord('xd')(message.content.lower()):
-            await self.bot.send_typing(message.channel)
+            self.bot.send_typing(message.channel)
             self.bot.striggers += 1
             self.bot.utriggers += 1
-            await self.bot.send_message(message.channel, '<a:xd:442034831690301461>')
+            self.bot.send_message(message.channel, '<a:xd:442034831690301461>')
 
         if '🤔' in message.content:
-            await self.bot.send_typing(message.channel)
+            self.bot.send_typing(message.channel)
             self.bot.striggers += 1
             self.bot.utriggers += 1
-            await self.bot.send_message(message.channel, embed=emThink)
+            self.bot.send_message(message.channel, embed=emThink)
 
         if 'no u' in message.content.lower():
-            await self.bot.send_typing(message.channel)
+            self.bot.send_typing(message.channel)
             self.bot.striggers += 1
             self.bot.utriggers += 1
-            await self.bot.send_message(message.channel, 'Ladies and gentlmen, <@' + message.author.id + '> appears to have won this argument. You can stop fighting like little cucklets now.') #CBP
+            self.bot.send_message(message.channel, 'Ladies and gentlmen, <@' + message.author.id + '> appears to have won this argument. You can stop fighting like little cucklets now.') #CBP
