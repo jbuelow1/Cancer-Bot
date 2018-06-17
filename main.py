0d1fdf928a7c9c30d32e645fc7000b7ef5ef1f03
jbuelow1 <jdbuelow@gmail.com>
2018-06-17 15:11:58 -0500

    fix?

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ helpStati = [
 debug = True
 
 bot = discord.Client()
-parser = modules.parser.parser(bot)
+parser = modules.parser(bot)
 
 emBleach = discord.Embed(title=''.join((dancefont['k'],dancefont['y'],dancefont['s'])), color=0x00ff00) #CBP
 emBleach.set_image(url="https://i.imgur.com/Mto46BE.png")
