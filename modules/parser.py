ebcf9565ed5f5d6b2b97dad0cd0f4dc4028a0b76
Yamcha <jdbuelow@gmail.com>
2018-06-17 19:15:55 -0500

    testing ?/;reload

--- modules/parser.py
+++ modules/parser.py
@@ -75,7 +75,7 @@ class Mparser:
         return True if re.search(r'\b' + word + r'\b', string_value) else False
 
     async def commands(self, message):
-        pass
+        await self.bot.send_typing(message.channel)
 
     async def triggers(self, message):
         print("starting trigger parser...")
