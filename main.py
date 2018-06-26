cf367eee373686eb187469aca7d2653345904a3f
Yamcha <jdbuelow@gmail.com>
2018-06-26 12:41:22 -0500

    testing trigger cog (ported)

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ def get_prefix(bot, message):
 # Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
 # Think of it like a dot path import
 initial_extensions = [
-'cogs.owner'
+'cogs.owner',
+'cogs.trigger'
 ]
 
 bot = commands.Bot(command_prefix=get_prefix, description='A Rewrite Cog Example')
