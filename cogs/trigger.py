318d1d58f4d11b7821b7afa8bf9c239f80f6371f
Yamcha <jdbuelow@gmail.com>
2018-06-27 09:57:20 -0500

    wordinstring

--- cogs/trigger.py
+++ cogs/trigger.py
@@ -71,6 +71,9 @@ class triggerCog:
         '9': '9'
         }
 
+    def wordInString(self, word, string_value):
+        return True if re.search(r'\b' + word + r'\b', string_value) else False
+
     async def on_message(self, message):
         print("starting trigger parser...")
         emBleach = discord.Embed(title=''.join((self.dancefont['k'],self.dancefont['y'],self.dancefont['s'])), color=0x00ff00) #CBP
