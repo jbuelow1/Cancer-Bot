d7fbe640544d0647c95210e51fb936a4d0bef58c
Yamcha <jdbuelow@gmail.com>
2018-06-27 09:54:08 -0500

    added to init

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def get_prefix(bot, message):
 # Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
 # Think of it like a dot path import
 initial_extensions = [
+'cogs.help',
 'cogs.owner',
 'cogs.trigger'
 ]
