953c351fcd27f0be9a77dee0f2cef814ffa48b6f
Yamcha <jdbuelow@gmail.com>
2018-06-27 10:33:10 -0500

    re-did owner (re-copied)

--- cogs/owner.py
+++ cogs/owner.py
@@ -8,6 +8,7 @@ class OwnerCog:
 
     # Hidden means it won't show up on the default help.
     @commands.command(name='load', hidden=True)
+    @commands.is_owner()
     async def cog_load(self, ctx, *, cog: str):
         """Command which Loads a Module.
         Remember to use dot path. e.g: cogs.owner"""
@@ -15,11 +16,12 @@ class OwnerCog:
         try:
             self.bot.load_extension(cog)
         except Exception as e:
-            await ctx.send('**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
             await ctx.send('**`SUCCESS`**')
 
     @commands.command(name='unload', hidden=True)
+    @commands.is_owner()
     async def cog_unload(self, ctx, *, cog: str):
         """Command which Unloads a Module.
         Remember to use dot path. e.g: cogs.owner"""
@@ -27,11 +29,12 @@ class OwnerCog:
         try:
             self.bot.unload_extension(cog)
         except Exception as e:
-            await ctx.send('**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
             await ctx.send('**`SUCCESS`**')
 
     @commands.command(name='reload', hidden=True)
+    @commands.is_owner()
     async def cog_reload(self, ctx, *, cog: str):
         """Command which Reloads a Module.
         Remember to use dot path. e.g: cogs.owner"""
@@ -40,7 +43,7 @@ class OwnerCog:
             self.bot.unload_extension(cog)
             self.bot.load_extension(cog)
         except Exception as e:
-            await ctx.send('**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
             await ctx.send('**`SUCCESS`**')
 
