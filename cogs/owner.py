adf7475540c3d458a858741db52c8ed5eef94517
Yamcha <jdbuelow@gmail.com>
2018-06-27 21:32:13 -0500

    more selfing

--- cogs/owner.py
+++ cogs/owner.py
@@ -6,12 +6,12 @@ class ownerCog:
         self.bot = bot
 
     async def on_server_join(self, server):
-        owner = await bot.get_user_info('273940917596061698')
+        owner = await self.bot.get_user_info('273940917596061698')
         await owner.send('**HEWWO SENPAI I HAS JOINED A NEW SERVER CALLED** ' + server.name + ' **WITH** ' + str(len(server.members)) + ' **MEMBERS!**')
 
     @commands.command(name='suggest')
     async def suggest(self, ctx, *, arg):
-        owner = await bot.get_user_info('273940917596061698')
+        owner = await self.bot.get_user_info('273940917596061698')
         await owner.send('HEWWO SENPAI I HAS FEEDBACK FROM `' + str(ctx.message.author) + '`:```' + arg.replace('```', '<REMOVED>') + '```')
         await ctx.send('Thanks for your feedback! Senpai has been notified!')
 
