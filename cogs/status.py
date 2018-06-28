9129f780aedb39a5fa424843340b82a1eda8c49e
Yamcha <jdbuelow@gmail.com>
2018-06-27 21:41:05 -0500

    selfing

--- cogs/status.py
+++ cogs/status.py
@@ -52,7 +52,7 @@ class statusCog:
 
     def __init__(self, bot):
         self.bot = bot
-        self.bot.loop.create_task(status_change())
+        self.bot.loop.create_task(self.status_change())
 
 def setup(bot):
     bot.add_cog(statusCog(bot))
