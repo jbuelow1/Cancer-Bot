dcfca21db660ee5074e810ab8908e9924b7f63d6
Yamcha <jdbuelow@gmail.com>
2018-06-27 21:29:32 -0500

    added owner cog

---
+++ cogs/owner.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+import discord
+
+class ownerCog:
+    def __init__(bot):
+        self.bot = bot
+
+    async def on_server_join(self, server):
+        owner = await bot.get_user_info('273940917596061698')
+        await owner.send('**HEWWO SENPAI I HAS JOINED A NEW SERVER CALLED** ' + server.name + ' **WITH** ' + str(len(server.members)) + ' **MEMBERS!**')
+
+    @commands.command(name='suggest')
+    async def suggest(self, ctx, *, arg):
+        owner = await bot.get_user_info('273940917596061698')
+        await owner.send('HEWWO SENPAI I HAS FEEDBACK FROM `' + str(ctx.message.author) + '`:```' + arg.replace('```', '<REMOVED>') + '```')
+        await ctx.send('Thanks for your feedback! Senpai has been notified!')
