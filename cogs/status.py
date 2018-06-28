e8ff46b2f90b5f9fd09e028ae6add6cf538416ce
Yamcha <jdbuelow@gmail.com>
2018-06-27 21:38:35 -0500

    added status changer cog

---
+++ cogs/status.py
@@ -0,0 +1,58 @@
+from discord.ext import commands
+import discord
+
+import random
+import asyncio
+
+class statusCog:
+    async def status_change():
+        stati = [
+        'with little children', #CBP
+        'with ur mum XD', #CBP
+        '( ͡° ͜ʖ ͡°)',
+        'with big, big balls', #CBP
+        'FORTNITE XD XD',
+        'yeeting babies', #CBP
+        'deepfrying the memes',
+        'bepis simulator',
+        'with myself', #CBP
+        'with my peepee', #CBP
+        'with mommy\'s peepee', #CBP
+        'midget basketball', #CBP
+        'with my rocket', #CBP
+        'with the anthros', #CBP
+        'peek a boo ( ͡° ͜ʖ ͡°)', #CBP
+        'on e621', #CBP
+        'lewding lolis', #CBP
+        'dead',
+        'alone',
+        'Discord',
+        'with high voltage',
+        'on a shitty server', #CBP
+        'in the street', #CBP
+        'with lives of the innocent', #CBP
+        'rm -rf /',
+        'with 14 werewolves', #CBP
+        'in a back alley' #CBP
+        ]
+
+        helpStati = [
+        'type ?/help',
+        'try ?/help',
+        'use ?/help',
+        'say ?/help',
+        '?/help'
+        ]
+
+        while True:
+            await self.bot.change_presence(game=discord.Game(name=random.choice(stati)))
+            await asyncio.sleep(20)
+            await self.bot.change_presence(game=discord.Game(name=random.choice(helpStati) + ' in ' + str(len(bot.servers) - 2) + ' servers'))
+            await asyncio.sleep(10)
+
+    def __init__(bot):
+        self.bot = bot
+        self.bot.loop.create_task(status_change())
+
+def setup(bot):
+    bot.add_cog(statusCog(bot))
