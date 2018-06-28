e332cea0e15b7192f162a104e8f0e59a17c9ca36
Yamcha <jdbuelow@gmail.com>
2018-06-28 15:05:04 -0500

    added unfortunate image command

--- cogs/images.py
+++ cogs/images.py
@@ -47,6 +47,13 @@ class imagesCog:
         done = Image.open(BytesIO(done))
         return done
 
+    def unfortunateProcess(self, image):
+        image = image.resize((350, 300))
+        unfortunate = Image.open('imgsrc/unfortunate.png')
+        unfortunate.paste(image, (625, 400))
+        return unfortunate
+
+
     @commands.command(name='jpeg')
     async def jpeg(self, ctx):
         async with ctx.typing():
@@ -109,6 +116,31 @@ class imagesCog:
                     for image in images:
                         image = self.unsharpenimg(image)
                         image = self.addjpeg(image)
+
+                        output = BytesIO()
+                        image.save(output, format="PNG")
+                        image = output.getvalue()
+                        output.close()
+
+                        outputImages.append(discord.File(BytesIO(image), filename='jpeg' + str(filenum) + '.jpeg'))
+                        filenum += 1
+                        print(outputImages)
+                    await ctx.send(':white_check_mark: Done! :white_check_mark:', files=outputImages)
+                else:
+                    await ctx.send(':warning: Too many files! Please supply 1-10 per message. :warning:')
+            else:
+                await ctx.send(':warning: Please supply a `.png`, `.jpg`/`.jpeg`, or `.bmp` image file! :warning:')
+
+    @commands.command(name='unfortunate')
+    async def unfortunate(self, ctx):
+        async with ctx.typing():
+            images = self.getImages(ctx.message)
+            if len(images) > 0:
+                if len(images) < 10:
+                    outputImages = []
+                    filenum = 0
+                    for image in images:
+                        image = self.unfortunateProcess(image)
 
                         output = BytesIO()
                         image.save(output, format="PNG")
