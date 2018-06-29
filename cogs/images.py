29246944618f4aff08765f48ffc157f071253e37
jbuelow1 <jdbuelow@gmail.com>
2018-06-29 11:23:23 -0500

    added 14rw command

--- cogs/images.py
+++ cogs/images.py
@@ -187,5 +187,31 @@ class imagesCog:
             else:
                 await ctx.send(':warning: Please supply a `.png`, `.jpg`/`.jpeg`, or `.bmp` image file! :warning:')
 
+    @commands.command(name='14rw')
+    @commands.cooldown(1, 10, commands.BucketType.channel)
+    async def unfortunate(self, ctx):
+        async with ctx.typing():
+            images = self.getImages(ctx.message)
+            if len(images) > 0:
+                if len(images) < 10:
+                    outputImages = []
+                    filenum = 0
+                    for image in images:
+                        image = self.picInPic(image, '14rw.png', (744, 484), (0, 255))
+
+                        output = BytesIO()
+                        image.save(output, format="PNG")
+                        image = output.getvalue()
+                        output.close()
+
+                        outputImages.append(discord.File(BytesIO(image), filename='jpeg' + str(filenum) + '.jpeg'))
+                        filenum += 1
+                        print(outputImages)
+                    await ctx.send(':white_check_mark: Done! :white_check_mark:', files=outputImages)
+                else:
+                    await ctx.send(':warning: Too many files! Please supply 1-10 per message. :warning:')
+            else:
+                await ctx.send(':warning: Please supply a `.png`, `.jpg`/`.jpeg`, or `.bmp` image file! :warning:')
+
 def setup(bot):
     bot.add_cog(imagesCog(bot))
