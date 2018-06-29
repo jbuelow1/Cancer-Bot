8368f6ebeb32f378552ae92f05da4e8ed62df54e
jbuelow1 <jdbuelow@gmail.com>
2018-06-29 10:39:15 -0500

    better rescaling

--- cogs/images.py
+++ cogs/images.py
@@ -47,9 +47,43 @@ class imagesCog:
         done = Image.open(BytesIO(done))
         return done
 
+    def rescale(data, width, height, force=True):
+    	"""Rescale the given image, optionally cropping it to make sure the result image has the specified width and height."""
+    	import Image as pil
+    	from cStringIO import StringIO
+
+    	max_width = width
+    	max_height = height
+
+    	input_file = StringIO(data)
+    	img = pil.open(input_file)
+    	if not force:
+    		img.thumbnail((max_width, max_height), pil.ANTIALIAS)
+    	else:
+    		src_width, src_height = img.size
+    		src_ratio = float(src_width) / float(src_height)
+    		dst_width, dst_height = max_width, max_height
+    		dst_ratio = float(dst_width) / float(dst_height)
+
+    		if dst_ratio < src_ratio:
+    			crop_height = src_height
+    			crop_width = crop_height * dst_ratio
+    			x_offset = float(src_width - crop_width) / 2
+    			y_offset = 0
+    		else:
+    			crop_width = src_width
+    			crop_height = crop_width / dst_ratio
+    			x_offset = 0
+    			y_offset = float(src_height - crop_height) / 3
+    		img = img.crop((x_offset, y_offset, x_offset+int(crop_width), y_offset+int(crop_height)))
+    		img = img.resize((dst_width, dst_height), pil.ANTIALIAS)
+
+    	return img
+
+
     def picInPic(self, image, background, size, location):
         image = image.convert('RGBA')
-        image = image.resize(size)
+        image = self.rescale(image, size[0], size[1], False)
         background = Image.open('imgsrc/' + background)
         background.paste(image, location, mask=image)
         return background
