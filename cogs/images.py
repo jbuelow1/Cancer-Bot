96e8a256bc361e40eac85fa1c7fd1fc2afd6f8da
jbuelow1 <jdbuelow@gmail.com>
2018-06-30 17:58:47 -0500

    final wrapping width

--- cogs/images.py
+++ cogs/images.py
@@ -228,7 +228,7 @@ class imagesCog:
             textarea = Image.new('RGBA', (344, 276), (0,0,0,0))
             font = ImageFont.truetype('fonts/facts.ttf', 30)
             d = ImageDraw.Draw(textarea)
-            lines = textwrap.wrap(text, width=20)
+            lines = textwrap.wrap(text, width=26)
             if len(lines) > 7:
                 await ctx.send(':warning: Too much text! :warning:')
                 return
