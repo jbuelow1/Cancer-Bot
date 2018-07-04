56774c06ce930b19b2aafeea6d87f5764d8bc082
jbuelow1 <jdbuelow@gmail.com>
2018-07-03 22:53:10 -0500

    more debugging

--- cogs/imagestest.py
+++ cogs/imagestest.py
@@ -27,6 +27,7 @@ class imagestestCog:
                 print('No images found in invoke message. Searching history...')
                 channel = ctx.message.channel
                 messages = await channel.history(limit=25).flatten()
+                print(str(messages))
                 for message in messages:
                     print('Searching message #' + message.id + ' by ' + str(message.author))
                     images = self.getImages(message)
