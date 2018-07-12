07608ccb3826f6513b2e5b0294d96e123dfe0693
Yamcha <jdbuelow@gmail.com>
2018-07-12 16:30:31 -0500

    fixing statistics

--- cogs/stats.py
+++ cogs/stats.py
@@ -11,21 +11,23 @@ class statsCog:
     def __init__(self, bot):
         self.bot = bot
 
-    async def save_stats():
-        while True:
-            try:
-                with open('actions.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
-                    self.bot.vcommands, self.bot.vtriggers = pickle.load(f)
-            except:
-                self.bot.vcommands = 0
-                self.bot.vtriggers = 0
+        async def save_stats():
+            while True:
+                try:
+                    with open('actions.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+                        self.bot.vcommands, self.bot.vtriggers = pickle.load(f)
+                except:
+                    self.bot.vcommands = 0
+                    self.bot.vtriggers = 0
 
-            with open('actions.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
-                pickle.dump([self.bot.ucommands + self.bot.vcommands, self.bot.utriggers + self.bot.vtriggers], f)
+                with open('actions.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+                    pickle.dump([self.bot.ucommands + self.bot.vcommands, self.bot.utriggers + self.bot.vtriggers], f)
 
-            self.bot.ucommands = 0
-            self.bot.utriggers = 0
-            await asyncio.sleep(60)
+                self.bot.ucommands = 0
+                self.bot.utriggers = 0
+                await asyncio.sleep(60)
+
+        self.bot.loop.create_task(save_stats())
 
     async def on_command(self, ctx):
         self.bot.scommands += 1
@@ -56,8 +58,6 @@ class statsCog:
         emStats.add_field(name='Triggers', value=self.bot.vtriggers + self.bot.utriggers, inline=True)
         emStats.set_footer(icon_url=ctx.message.author.avatar_url, text=str(ctx.message.author.display_name) + ' requested this command')
         await ctx.send(embed=emStats)
-
-    self.bot.loop.create_task(save_stats())
 
 def setup(bot):
     bot.add_cog(statsCog(bot))
