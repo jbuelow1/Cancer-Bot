8a492d13c0e3e353030ae859dc31a77251d460e4
jbuelow1 <jdbuelow@gmail.com>
2018-07-20 16:10:21 -0500

    made vote checking a check

--- cogs/copypasta.py
+++ cogs/copypasta.py
@@ -5,6 +5,7 @@ import os
 import requests
 import textwrap
 import configparser
+import ast
 
 class copypastaCog:
     def __init__(self, bot):
@@ -17,23 +18,26 @@ class copypastaCog:
         else:
             print('Could not find a config file for dbl. HOW THE FUCK AM I RUNNING????')
 
+    async def has_voted(self, ctx):
+        headers = {'Content-Type': 'application/json'}
+        headers['Authorization'] = self.dbltoken
+        r = requests.get('https://discordbots.org/api/bots/439851454203691019/check?userId=' + str(ctx.author.id), headers=headers)
+        if r.status_code == 200:
+            rdict = ast.literal_eval(r.json())
+            if rdict['voted'] == 1:
+                return True
+            if rdict['voted'] == 0:
+                await ctx.send(':no_entry_sign: Woah there! Seems like you havent voted today! Try using this command again once you have voted. :no_entry_sign:\nVote for Cancer Bot at: https://discordbots.org/bot/439851454203691019/vote')
+                return False
+
     @commands.command(category='copypasta', name='fit', usage='', brief='Gotta stay fit, kids')
     async def fitPasta(self, ctx):
         await ctx.send('**The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.**')
 
     @commands.command(category='copypasta', name='bee', usage='', brief='why? (spam warning)')
     @commands.cooldown(1, 300, commands.BucketType.channel)
+    @commands.check(has_voted)
     async def beePasta(self, ctx):
-        print('checking for users upvote status...')
-        print(str(ctx.author.id))
-        print(self.dbltoken)
-        headers = {'Content-Type': 'application/json'}
-        headers['Authorization'] = self.dbltoken
-        r = requests.get('https://discordbots.org/api/bots/439851454203691019/check?userId=' + str(ctx.author.id), headers=headers)
-        print("done")
-        if r.status_code == 200:
-            print(r.json())
-        print(str(voted))
         f = open('copypastas/bee.txt', mode='r')
         pasta = f.read()
         lines = textwrap.wrap(pasta, width=1990)
@@ -41,6 +45,7 @@ class copypastaCog:
             await ctx.send('**' + line + '**')
             await ctx.trigger_typing()
         await ctx.send('That is all.')
+
 
 def setup(bot):
     bot.add_cog(copypastaCog(bot))
