68970807a2aa62433139e7ae41645a3cecf87d5c
jbuelow1 <jdbuelow@gmail.com>
2018-07-20 15:19:00 -0500

    added traceback printing for owner

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 import discord
 
+import traceback
+
 class errorCog:
     def __init__(self, bot):
         self.bot = bot
@@ -26,6 +28,9 @@ class errorCog:
             for perm in error.missing_perms:
                 perms = perms + '\n' + str(perm)
             await ctx.send(':no_entry_sign: Sorry, ' + ctx.author.mention + ', but I do not have sufficient permissions to do that here. Contact an admin of this server for help. :no_entry_sign:\n```Missing Permissions for ' + self.bot.user.nick + ':' + perms + '```')
+
+        if await self.bot.is_owner(ctx.author):
+            await ctx.send(':warning: An error has occoured in that command! :warning:\nTraceback:```' + traceback.format_exc() + '```')
 
 def setup(bot):
     bot.add_cog(errorCog(bot))
