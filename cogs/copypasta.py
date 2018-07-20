416860b1a7fa71f21a6f2e9b7ed9b73b058affab
jbuelow1 <jdbuelow@gmail.com>
2018-07-19 23:40:46 -0500

    more printing

--- cogs/copypasta.py
+++ cogs/copypasta.py
@@ -25,6 +25,8 @@ class copypastaCog:
     @commands.cooldown(1, 300, commands.BucketType.channel)
     async def beePasta(self, ctx):
         print('checking for users upvote status...')
+        print('user id: ' + ctx.author.id)
+        print(self.dbltoken)
         headers = {'Content-Type': 'application/json', 'Authorization': self.dbltoken}
         r = requests.get('https://discordbots.org/api/bots/439851454203691019/check?userId=' + ctx.author.id, headers=headers)
         print("done")
