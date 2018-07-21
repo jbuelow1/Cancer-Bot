62ed18b8bef91950fd2bcd0f80553b193859374c
jbuelow1 <jdbuelow@gmail.com>
2018-07-21 16:01:31 -0500

    preventing 8 terabyte logfiles

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ async def on_ready():
     print('Successfully logged in and booted...!')
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
