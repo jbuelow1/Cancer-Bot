8f171a2b2772d9e9b7abefd2a1a42a51caed98bb
jbuelow1 <jdbuelow@gmail.com>
2018-07-20 22:10:42 -0500

    ?/lsguilds now prints guild ids

--- cogs/owner.py
+++ cogs/owner.py
@@ -34,7 +34,7 @@ class ownerCog:
     async def lsguilds(self, ctx):
         guildList = ''
         for guild in self.bot.guilds:
-            guildList = guildList + guild.name + '\n'
+            guildList = guildList + str(guild.id) + ' | ' + guild.name + '\n'
         emLsGuilds = discord.Embed(title='Cancer Bot\'s Guild List', description=guildList, color=0xffff00)
         await ctx.send(embed=emLsGuilds)
 
