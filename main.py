d808c1b529fac22d0c3c9b1e923c44b7cab287c8
jbuelow1 <jdbuelow@gmail.com>
2018-07-21 15:31:59 -0500

    stealing unableimp's code that he stole from the example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+import logging
 import os, sys, traceback
 from six.moves import configparser
 import datetime
@@ -75,6 +76,12 @@ async def on_ready():
     # Changes our bots Playing Status. type=1(streaming) for a standard game you could remove type and url.
     await bot.change_presence(game=discord.Game(name='Status loop not running'))
     print('Successfully logged in and booted...!')
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 filename = "ifr.cfg"
 if os.path.isfile(filename):
