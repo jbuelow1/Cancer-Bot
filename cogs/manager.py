2b350f3e4a31db8004440f86e55bebea05f862a5
Yamcha <jdbuelow@gmail.com>
2018-08-14 16:10:38 -0500

    bug fixes and transition to embeds

--- cogs/manager.py
+++ cogs/manager.py
@@ -26,7 +26,7 @@ class managerCog:
         ]
         return ctx.author.id in devs
 
-    # Hidden means it won't show up on the default help.
+    '''# Hidden means it won't show up on the default help.
     @commands.command(name='load', hidden=True)
     @commands.is_owner()
     async def cog_load(self, ctx, *, cog: str):
@@ -66,12 +66,14 @@ class managerCog:
         except Exception as e:
             await ctx.send('**ERROR:** `' + str(e) + '`')
         else:
-            await ctx.send('**SUCCESS:** `Unloaded module named \'' + cog + '\'`\n**SUCCESS:** `Loaded module named \'' + cog + '\'`')
+            await ctx.send('**SUCCESS:** `Unloaded module named \'' + cog + '\'`\n**SUCCESS:** `Loaded module named \'' + cog + '\'`')'''
 
     @commands.group(name='modman', hidden=True, invoke_without_command=True)
     @commands.check(is_dev)
     async def modman(self, ctx):
-        await ctx.send('modman requires an action.')
+        e = discord.Embed(title='No action specified', description='You can use:\n`load`\n`unload`\n`reload`\n`clear`\n~~`list`~~', color=0xff0000)
+        e.set_author(name='Cancer Bot Module Manager', icon_url=self.bot.user.avatar_url)
+        await ctx.send(embed=e)
 
     @modman.command(name='load', hidden=True)
     @commands.check(is_dev)
@@ -79,9 +81,14 @@ class managerCog:
         try:
             self.bot.load_extension(cog)
         except Exception as e:
-            await ctx.send('**ERROR:** `' + str(e) + '`')
+            e = discord.Embed(title='Could not load module', description='Python error while loading module ' + cog + ':\n```' + str(e) + '```', color=0xff0000)
+            e.set_author(name='Cancer Bot Module Manager', icon_url=self.bot.user.avatar_url)
+            await ctx.send(embed=e)
         else:
-            await ctx.send('**SUCCESS:** `Loaded module named \'' + cog + '\'`')
+            e = discord.Embed(title='Module Loaded', color=0x00ff00)
+            e.set_author(name='Cancer Bot Module Manager', icon_url=self.bot.user.avatar_url)
+            e.add_field(name='Module', value=cog)
+            await ctx.send(embed=e)
 
     @modman.command(name='unload', hidden=True)
     @commands.check(is_dev)
@@ -89,9 +96,14 @@ class managerCog:
         try:
             self.bot.unload_extension(cog)
         except Exception as e:
-            await ctx.send('**ERROR:** `' + str(e) + '`')
+            e = discord.Embed(title='Could not unload module', description='Python error while unloading module ' + cog + ':\n```' + str(e) + '```', color=0xff0000)
+            e.set_author(name='Cancer Bot Module Manager', icon_url=self.bot.user.avatar_url)
+            await ctx.send(embed=e)
         else:
-            await ctx.send('**SUCCESS:** `Unloaded module named \'' + cog + '\'`')
+            e = discord.Embed(title='Module Unloaded', color=0x00ff00)
+            e.set_author(name='Cancer Bot Module Manager', icon_url=self.bot.user.avatar_url)
+            e.add_field(name='Module', value=cog)
+            await ctx.send(embed=e)
 
     @modman.command(name='reload', hidden=True)
     @commands.check(is_dev)
@@ -100,9 +112,14 @@ class managerCog:
             self.bot.unload_extension(cog)
             self.bot.load_extension(cog)
         except Exception as e:
-            await ctx.send('**ERROR:** `' + str(e) + '`')
+            e = discord.Embed(title='Could not reload module', description='Python error while reloading module ' + cog + ':\n```' + str(e) + '```', color=0xff0000)
+            e.set_author(name='Cancer Bot Module Manager', icon_url=self.bot.user.avatar_url)
+            await ctx.send(embed=e)
         else:
-            await ctx.send('**SUCCESS:** `Unloaded module named \'' + cog + '\'`\n**SUCCESS:** `Loaded module named \'' + cog + '\'`')
+            e = discord.Embed(title='Module Reloaded', color=0x00ff00)
+            e.set_author(name='Cancer Bot Module Manager', icon_url=self.bot.user.avatar_url)
+            e.add_field(name='Module', value=cog)
+            await ctx.send(embed=e)
 
     @modman.command(name='clear', hidden=True)
     @commands.check(is_dev)
@@ -110,11 +127,15 @@ class managerCog:
         try:
             shutil.rmtree('cogs/__pycache__')
         except Exception as e:
-            await ctx.send('**ERROR:** `' + str(e) + '`')
+            e = discord.Embed(title='Could not clear cache', description='Python error while clearing cache:\n```' + str(e) + '```', color=0xff0000)
+            e.set_author(name='Cancer Bot Module Manager', icon_url=self.bot.user.avatar_url)
+            await ctx.send(embed=e)
         else:
-            await ctx.send('**SUCCESS:** `Cache folder has been cleared.`')
+            e = discord.Embed(title='Cache cleared', color=0x00ff00)
+            e.set_author(name='Cancer Bot Module Manager', icon_url=self.bot.user.avatar_url)
+            await ctx.send(embed=e)
 
-    @modman.group(name='list', hidden=True, invoke_without_command=True)
+    '''@modman.group(name='list', hidden=True, invoke_without_command=True)
     @commands.check(is_dev)
     async def modmanList(self, ctx):
         await ctx.send('You must specify ~~`all`~~, `avaliable` or ~~`loaded`~~.')
@@ -126,7 +147,7 @@ class managerCog:
         for file in os.listdir('./cogs'):
             if file.endswith('.py'):
                 mods += file.replace(file.split('.')[-1], '').replace('/','.') + '\n'
-        await ctx.send('Module listing complete:\n```' + mods + '```')
+        await ctx.send('Module listing complete:\n```' + mods + '```')'''
 
 def setup(bot):
     bot.add_cog(managerCog(bot))
