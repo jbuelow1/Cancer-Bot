8af6db6859cbbf4f45de823c7bf6f16b5537d077
Yamcha <jdbuelow@gmail.com>
2018-08-15 11:15:21 -0500

    added universal error message and CommandNotFound error

--- cogs/errors.py
+++ cogs/errors.py
@@ -9,22 +9,32 @@ class errorCog:
         self.bot = bot
 
     async def on_command_error(self, ctx, error):
+        handled = False
         if isinstance(error, commands.CommandOnCooldown):
+            handled = True
             await ctx.send(':no_entry_sign: ' + ctx.author.mention + ', that command is on cooldown. Please try again in ' + str(error.retry_after) + ' seconds. :no_entry_sign:')
 
         if isinstance(error, commands.DisabledCommand):
+            handled = True
             await ctx.send(':no_entry_sign: ' + ctx.author.mention + ', that command is currently disabled. Please try again later. :no_entry_sign:')
 
+        if isinstance(error, commands.CommandNotFound):
+            handled = True
+            await ctx.send(':no_entry_sign: That command does not exist! You can use the `?/suggest` command to tell senpai you want this command. :no_entry_sign:')
+
         if isinstance(error, commands.NotOwner):
+            handled = True
             await ctx.send(':raised_hand: HALT! That command is for senpai only!')
 
         if isinstance(error, commands.MissingPermissions):
+            handled = True
             perms = ''
             for perm in error.missing_perms:
                 perms = perms + '\n' + str(perm)
             await ctx.send(':no_entry_sign: Sorry, ' + ctx.author.mention + ', but you do not have sufficient permissions to do that here. Contact an admin of this server for help. :no_entry_sign:\n```Missing Permissions for ' + ctx.author.nick + ':' + perms + '```')
 
         if isinstance(error, commands.BotMissingPermissions):
+            handled = True
             perms = ''
             for perm in error.missing_perms:
                 perms = perms + '\n' + str(perm)
@@ -32,6 +42,9 @@ class errorCog:
 
         if await self.bot.is_owner(ctx.author):
             await ctx.send(':warning: An error has occoured in that command! :warning:\nTraceback:```' + str(error) + '```')
+
+        if not handled:
+            await ctx.send('OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!\nˢᵉʳᶦᵒᵘˢˡʸ ⁿᵒᵗ ᵃ ᶠᵘᶜᵏᶦⁿᵍ ᶜˡᵘᵉ ʷʰᵃᵗ ʷᵉⁿᵗ ʷʳᵒⁿᵍ ʰᵉʳᵉ')
 
 def setup(bot):
     bot.add_cog(errorCog(bot))
