9f3b2cbc0010d11cf7ea2ef9cb00c83fb6538a67
Yamcha <jdbuelow@gmail.com>
2018-08-22 11:06:57 -0500

    testing

--- cogs/basic.py
+++ cogs/basic.py
@@ -143,8 +143,14 @@ class basicCog:
                         users.remove(member.id)
 
             counts = Counter(games)
-            values = [counts['Online'], counts['Offline'], counts['Idle'], counts['Do Not Disturb'], counts['Other']]
-            labels = ['Online' + '\n(' + str(counts['Online']) + ')', 'Offline' + '\n(' + str(counts['Offline']) + ')', 'Idle' + '\n(' + str(counts['Idle']) + ')', 'Do Not Disturb' + '\n(' + str(counts['Do Not Disturb'], 'Other' + '\n(' + str(counts['Other']) + ')']) + ')']
+            values = [counts['Online'], counts['Offline'], counts['Idle'], counts['Do Not Disturb']]
+            '''labels = [
+            'Online' + '\n(' + str(counts['Online']) + ')',
+            'Offline' + '\n(' + str(counts['Offline']) + ')',
+            'Idle' + '\n(' + str(counts['Idle']) + ')',
+            'Do Not Disturb' + '\n(' + str(counts['Do Not Disturb']
+            ]'''
+            labels = ['Online', 'Offline', 'Idle', 'Do Not Disturb']
             colors = ['green', 'grey', 'orange', 'red', 'black']
             plt.pie(values, labels=labels, colors=colors autopct=None)
             f = io.BytesIO()
